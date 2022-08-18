a2ace2a36159519e47e260d86989e3541fc6b8e2
Anik Majumder <anikmajumder1592@gmail.com>
2022-08-18 12:16:46 +0600

    first commit

---
+++ js/bank.js
@@ -0,0 +1,45 @@
+document.getElementById("btn-deposit").addEventListener("click", function () {
+  // deposit calculation
+  const dipositField = document.getElementById("diposit-input");
+  const newDipositAmount = dipositField.value;
+
+  const dipositElement = document.getElementById("deposit");
+  const previousDipositTotal = dipositElement.innerText;
+  const currentDipositTotal =
+    parseFloat(newDipositAmount) + parseFloat(previousDipositTotal);
+  dipositElement.innerText = currentDipositTotal;
+  dipositField.value = "";
+
+  //   total calculation after deposit
+
+  const balanceTotalElement = document.getElementById("balance");
+  const previousBalanceTotalString = balanceTotalElement.innerText;
+  const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+  const currentBalanceTotal =
+    previousBalanceTotal + parseFloat(newDipositAmount);
+  balanceTotalElement.innerText = currentBalanceTotal;
+});
+
+document.getElementById("btn-withdraw").addEventListener("click", function () {
+  // withdraw calculation
+  const withdrawField = document.getElementById("withdraw-input");
+  const newWithdrawAmount = withdrawField.value;
+
+  const withdrawElement = document.getElementById("withdraw");
+  const previousWithdrawTotal = withdrawElement.innerText;
+  const currentWithdrawTotal =
+    parseFloat(newWithdrawAmount) + parseFloat(previousWithdrawTotal);
+  withdrawElement.innerText = currentWithdrawTotal;
+  withdrawField.value = "";
+
+  //   total calculation after deposit
+
+  const balanceTotalElement = document.getElementById("balance");
+  const previousBalanceTotalString = balanceTotalElement.innerText;
+  const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+  const currentBalanceTotal =
+    previousBalanceTotal - parseFloat(newWithdrawAmount);
+  balanceTotalElement.innerText = currentBalanceTotal;
+});
